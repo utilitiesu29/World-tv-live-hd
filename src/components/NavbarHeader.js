38d186e435b85cdbedfc4cea21dead8b22006e88
Begad <64358501+begadsalah@users.noreply.github.com>
2022-03-24 14:57:41 +0200

    11

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -22,10 +22,10 @@ const NavbarHeader = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/">About</Nav.Link>
-                            <Nav.Link href="/">Contact</Nav.Link>
-                            <Nav.Link href="/">Privacy policy</Nav.Link>
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#">About</Nav.Link>
+                            <Nav.Link href="#">Contact</Nav.Link>
+                            <Nav.Link href="#">Privacy policy</Nav.Link>
                             <NavDropdown title="Channels" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#international">International</NavDropdown.Item>
                                 <NavDropdown.Item href="#pakistan">Pakistan</NavDropdown.Item>
