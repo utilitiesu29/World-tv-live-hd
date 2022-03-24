7555ff2980704c0c9e229af92939bcc68e58a4b1
Begad <64358501+begadsalah@users.noreply.github.com>
2022-03-24 13:59:24 +0200

    6 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import LiveVideos from './components/LiveVideos';
 import NavbarHeader from './components/NavbarHeader';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from './components/About';
+import Contact from './components/Contact';
+import Policies from './components/Policies';
+import Home from './components/Home';
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavbarHeader />
-      <LiveVideos />
-    </div>
+      <Routes>
+        {/*<Route path="/Home" element={<Home />}></Route>*/}
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/Policies" element={<Policies />}></Route>
+        <Route path="/" element={<LiveVideos />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Policies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Policies = () => {
+    return (
+        <div><h1>PRIVACY POLICY</h1></div>
+    )
+}
+
+export default Policies
